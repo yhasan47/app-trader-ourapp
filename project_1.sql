aaf5754a1d7a58604fb0d70a5bb3b057528bf004
unknown <WMcWilliams15@gmail.com>
2020-01-23 20:56:48 -0600

    first entry on genres

--- project_1.sql
+++ project_1.sql
@@ -1,6 +1,30 @@
-SELECT a.primary_genre, p.name, a.name, p.genres, p.price, a.price, p.content_rating, a.content_rating
+/*App Trader will purchase apps for 10,000 times the price of the app. 
+For apps that are priced from free up to $1.00, the purchase price is $10,000.
+
+
+SELECT  p.name, a.name, p.genres, a.primary_genre, p.price, a.price, p.content_rating, a.content_rating
 FROM play_store_apps AS p
 INNER JOIN app_store_apps AS a
 ON p.name = a.name
-WHERE a.primary_genre = 'Business'
-ORDER BY a.primary_genre;      
+ORDER BY p.price DESC;
+
+only 79 are not free
+*/
+
+/*
+what genres are the most popular?
+
+How many categories of genres?
+Play_store: 119.
+App_store:	23.
+*/
+
+/*
+SELECT DISTINCT(primary_genre)
+FROM app_store_apps
+ORDER BY primary_genre;
+
+SELECT DISTINCT(genres)
+FROM app_store_apps
+ORDER BY genres;
+*/
