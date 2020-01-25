72bd20125b29e008c5a9aefb30bb1a72f793d5b7
yhasan47 <59903154+yhasan47@users.noreply.github.com>
2020-01-25 13:00:52 -0600

    Update scripts.sql

--- scripts.sql
+++ scripts.sql
@@ -54,9 +54,61 @@ LEFT JOIN play_store_apps AS android
 ONE android.name = apple.name
 
 -- 7.	What are the prices of the apps in both tables?
+SELECT p.price, p.name AS play_store, a.price, a.name AS app_store
+FROM play_store_apps AS p
+INNER JOIN app_store_apps AS a
+ON p.name = a.name
+ORDER BY a.price DESC;
+
+-- 8. 	What are some of the lowest priced apps from the Android app store with a rating of 5? 
+
+    SELECT name, price, rating
+    FROM play_store_apps
+    WHERE price = (SELECT MIN(price) FROM play_store_apps)
+    AND rating = 5
+    ORDER BY rating
+
+-- 9.	What are some apps from the Apple app store that are priced at either 0.99 cents or lower and have a rating of 5? 
+
+SELECT *
+FROM app_store_apps
+WHERE rating = 5.0 
+AND primary_genre LIKE 'Education'
+AND price <=0.99
+ORDER BY rating DESC
+LIMIT 100;
+
+
+--10.	Which apps from the app_store_apps have a price of 0 or 0.99 cent and are rated 5?
+
+SELECT *
+FROM app_store_apps
+WHERE (price = 0 OR price = 0.99)
+AND rating = 5
+
+--11.	How many apps are in both app stores?
+SELECT COUNT (apple.name)
+FROM app_store_apps AS apple
+LEFT JOIN play_store_apps AS android
+ONE android.name = apple.name
+
+--12.	What are the prices of the apps in both tables?
 
 SELECT p.price, p.name AS play_store, a.price, a.name AS app_store
 FROM play_store_apps AS p
 INNER JOIN app_store_apps AS a
 ON p.name = a.name
 ORDER BY a.price DESC;
+
+--13.	What are the names of apps that have duplicates in the app store?
+
+SELECT name, COUNT(name) 
+FROM app_store_apps
+GROUP BY name
+HAVING COUNT(name) > 1
+
+
+--14.	How many apps are duplicated in the play store?
+
+SELECT COUNT(name) - COUNT(DISTINCT name) AS duplicate_apps
+FROM play_store_apps;
