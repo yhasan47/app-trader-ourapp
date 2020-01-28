091fd1c47947b3b94a184fccbf36591ae1fa7357
unknown <WMcWilliams15@gmail.com>
2020-01-28 14:58:57 -0600

    first complete workthrough

--- will.sql
+++ will.sql
@@ -67,8 +67,8 @@ SELECT * FROM play_store_grouped;
 
 
 
-Looking at some of the dupes. They look like different apps made by different companies. 
-So I will keep them in the table.
+--Looking at some of the dupes. They look like different apps made by different companies. 
+--So I will keep them in the table.
 
 
 
@@ -87,7 +87,7 @@ ORDER BY name;
 
 
 
-Join the tables.
+--Join the tables.
 
 
 
@@ -109,11 +109,11 @@ ON p.name = a.name
 ORDER BY p.name;
 
 
-lets take a look at the prices. There is a $ in the playstore, making it a text attribute, so i cant do a comparison. 
-I have to convert to numeric attribute to make the columns compareable.
-30 apps have different prices.
-where there is a difference in price, looks like apple is the higher of the two.
-no major differences.
+--lets take a look at the prices. There is a $ in the playstore, making it a text attribute, so i cant do a comparison. 
+--I have to convert to numeric attribute to make the columns compareable.
+--30 apps have different prices.
+--where there is a difference in price, looks like apple is the higher of the two.
+--no major differences.
 
 
 SELECT p.name,
@@ -125,13 +125,13 @@ INNER JOIN app_store_apps AS a
 ON p.name = a.name
 WHERE CAST(REPLACE(p.price, '$', '') AS numeric) <> a.price
 ORDER BY p.name;
+*/
 
 
 
+--Lets take a look at the ratings. First lets make a temp table to make viewing the results easier.
 
-Lets take a look at the ratings. First lets make a temp table to make viewing the results easier.
-
-
+/*
 
 CREATE TEMP TABLE combined_table AS
 									SELECT 	p.name AS pname,
@@ -162,25 +162,195 @@ FROM combined_table;
 --WHERE rating::text NOT LIKE '%.0' AND				This way is will query exactly what i need.
 --	  rating::text NOT LIKE '%.5';
 
-
+/*
 SELECT arating, prating
 FROM combined_table;
-
+*/
 
 
 --Round playstore ratings so i can figure out the longevity. 
 /*
 ALTER TABLE play_store_grouped
 ADD COLUMN rating_rounded numeric;
-*/
--- JJJ
 
 
-/*
+
+
+
 UPDATE play_store_grouped
 SET rating_rounded = ROUND(ROUND(rating * 2, 0) / 2, 1);
+
+
+
+
+SELECT rating_rounded
+FROM play_store_grouped;
+
+
+SELECT 	p.name,
+		a.name,
+		p.rating_rounded,
+		a.rating,
+		p.price,
+		a.price,
+		p.genres,
+		a.primary_genre
+FROM play_store_grouped AS p
+INNER JOIN app_store_apps AS a
+ON a.name = p.name
+WHERE p.rating_rounded <> a.rating;
+
+----------------------------- started over again. picked up at cleaning the price and rating columns for easier comparisons---
+
+SELECT *
+FROM play_store_group;
+
+
+ALTER TABLE play_store_group
+ADD price_clean numeric (5,2)
+
+
+UPDATE play_store_group
+SET price_clean = (REPLACE(price, '$', ''))::numeric (5, 2);
+
+SELECT *
+FROM play_store_group
+ORDER BY price_clean DESC;
+
+/*
+
+CREATE TABLE playstore AS 
+SELECT name, rating, size, type, price, content_rating, genres
+FROM play_store_apps
+GROUP BY name, rating, size, type, price, content_rating, genres;
+
+ 
+ALTER TABLE playstore
+ADD price_clean numeric;
+
+UPDATE playstore
+SET price_clean = CAST(REPLACE(price, '$', '') AS numeric);
+
+SELECT *
+FROM playstore
+ORDER BY price_clean DESC;
+
+
+ALTER TABLE playstore
+ADD rating_rounded numeric;
+
+UPDATE playstore
+SET rating_rounded = ROUND((ROUND(rating * 2, 0) / 2), 1);
+
+
+SELECT rating, rating_rounded
+FROM playstore;
+
+
+
+SELECT 	p.name,
+		a.name,
+		p.price_clean,
+		a.price,
+		p.rating_rounded,
+		a.rating,
+		p.genres,
+		a.primary_genre,
+		p.content_rating,
+		a.content_rating
+FROM playstore AS p
+INNER JOIN app_store_apps AS a
+ON p.name = a.name
+ORDER BY p.name;
+
+
+CREATE TABLE joined_tables AS
+SELECT 	p.name AS play_name,
+		a.name AS apple_name,
+		p.price_clean AS play_price,
+		a.price AS apple_price,
+		p.rating_rounded AS play_rating,
+		a.rating AS apple_rating,
+		p.genres AS play_genres,
+		a.primary_genre AS apple_genres,
+		p.content_rating AS play_content_rating,
+		a.content_rating AS apple_content_rating
+FROM playstore AS p
+INNER JOIN app_store_apps AS a
+ON p.name = a.name
+ORDER BY p.name;
+
+
+SELECT *
+FROM joined_tables;
+
+
+ALTER TABLE joined_tables
+ADD greater_price numeric,
+ADD greater_rating numeric,
+ADD longevity numeric,
+ADD total_expense numeric,
+ADD total_revenue numeric,
+ADD total_profit numeric,
+ADD purchase_price numeric;
+
+
+UPDATE joined_tables
+SET greater_price = CASE 	WHEN play_price > apple_price 
+							THEN play_price 
+							ELSE apple_price END;
+
+UPDATE joined_tables
+SET greater_rating = CASE 	WHEN play_rating > apple_rating 
+							THEN play_rating 
+							ELSE apple_rating END;
+							
+
+SELECT play_price, apple_price, greater_price, play_rating, apple_rating, greater_rating
+FROM joined_tables
+ORDER BY play_price DESC;
+
+
+
+UPDATE joined_tables
+SET longevity = CASE 	WHEN greater_rating = 0.5 THEN 24
+						WHEN greater_rating = 1.0 THEN 36
+						WHEN greater_rating = 1.5 THEN 48
+						WHEN greater_rating = 2.0 THEN 60
+						WHEN greater_rating = 2.5 THEN 72
+						WHEN greater_rating = 3.0 THEN 84
+						WHEN greater_rating = 3.5 THEN 96
+						WHEN greater_rating = 4.0 THEN 108
+						WHEN greater_rating = 4.5 THEN 120
+						WHEN greater_rating = 5.0 THEN 132
+						ELSE 12 END;
+
+UPDATE joined_tables
+SET purchase_price = CASE	WHEN greater_price >= 1.0 
+							THEN greater_price * 10000
+							ELSE 10000 END;
+
+SELECT play_name, apple_name, play_price, apple_price, purchase_price, greater_rating, longevity
+FROM joined_tables
+ORDER BY play_price DESC;
+
+
+UPDATE joined_tables
+SET	total_expense = (longevity * 1000) + purchase_price,
+	total_revenue = longevity * 5000
+
+
+SELECT *
+FROM joined_tables;
+
+
+UPDATE joined_tables
+SET total_profit = total_revenue - total_expense;
 */
 
+SELECT *
+FROM joined_tables
+ORDER BY total_profit desc;
 
 
 
